a2d958913725684f04cc7a94f47ff97be74e3304
codeasf <code_asf@126.com>
2015-08-21 21:19:54 +0800

    add gps Display
    gps display not declear

--- GPS_DISPLAY_WEI.ino
+++ GPS_DISPLAY_WEI.ino
@@ -5,10 +5,10 @@ U8GLIB_ST7920_128X64_4X u8g(7);    // SPI Com: SCK = en = 13, MOSI = rw = 11, CS
 TinyGPSPlus gps;
 
 void draw(void) {
-  // graphic commands to redraw the complete screen should be placed here  
+  // graphic commands to redraw the complete screen should be placed here
   u8g.setFont(u8g_font_unifont);
   u8g.drawStr( 0, 10, "TOP:123.45KM/H");
-    
+
   u8g.setFont(u8g_font_fub49n);
   u8g.drawStr( 6, 63, "012");
 }
@@ -18,12 +18,17 @@ void setup(void) {
 }
 
 void loop(void) {
+    //GPS DISPLAY
+  while (*gpsStream)
+    if (gps.encode(*gpsStream++))
+      displayInfo();
+
   // picture loop
-  u8g.firstPage();  
+  u8g.firstPage();
   do {
     draw();
-  } while( u8g.nextPage() );
-  
+  } while ( u8g.nextPage() );
+
   // rebuild the picture after some delay
   delay(50);
 }
